27710efb34cd66a857a65931b1ae06de1a203823
duongdk099 <52460163+duongdk099@users.noreply.github.com>
2024-03-20 11:10:05 +0100

    fix read All Woods

--- controllers/wood.js
+++ controllers/wood.js
@@ -2,7 +2,7 @@ const { Wood } = require("../models/wood");
 exports.readAllWoods = (req, res) => {
   try {
     const woods = Wood.findAll();
-    res.json(woods);
+    res.status(200).json(woods);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
